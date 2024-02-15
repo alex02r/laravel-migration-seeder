363071bd1a8d34716ea432c978a49a9c2bb952e3
alex02r <132152331+alex02r@users.noreply.github.com>
2024-02-15 19:16:26 +0100

    [UPGRADE] Creata la funzione nel controller per il return della view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        //recuperiamo tutti i movie
+        $trains = Train::all();
+
+        return view('welcome', compact('trains'));
+    }
 }
