e39b6ba99df38539b034d0070929ea3a9ab53500
alex02r <132152331+alex02r@users.noreply.github.com>
2024-02-15 17:38:14 +0100

    [UPGRADE] Compilazione record nella tabella trains tramite seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+require_once 'vendor/autoload.php';
+
 
 class TrainSeeder extends Seeder
 {
@@ -12,8 +16,20 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            $new_train = new Train();
+            $new_train->company = $faker->company();
+            $new_train->start_staton = $faker->city();
+            $new_train->destination = $faker->city();
+            $new_train->start_time = $faker->time('H:i');
+            $new_train->end_time = $faker->time('H:i');
+            $new_train->code = $faker->randomNumber(5, true);
+            $new_train->carriages = $faker->randomNumber(2, false);
+            $new_train->late = $faker->boolean();
+            $new_train->deleted = $faker->boolean();
+
+        }
     }
 }
