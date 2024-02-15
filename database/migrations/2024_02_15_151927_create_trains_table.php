b64904e83fbe4878d6e253baaf3a4166d46d07b8
alex02r <132152331+alex02r@users.noreply.github.com>
2024-02-15 16:39:10 +0100

    [UPGRADE] Creazione della tabella

---
+++ database/migrations/2024_02_15_151927_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 80);
+            $table->string('start_station', 80);
+            $table->string('destination', 80);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('code', 5);
+            $table->tinyint('carriages');
+            $table->boolean('late');
+            $table->boolean('deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
