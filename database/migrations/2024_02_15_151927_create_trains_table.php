e4c83ff68496b9b19b8aa6ba13a2c677a5700b23
alex02r <132152331+alex02r@users.noreply.github.com>
2024-02-15 19:09:05 +0100

    [FIX] Aggiunto timespams tramite migrate rollback

--- database/migrations/2024_02_15_151927_create_trains_table.php
+++ database/migrations/2024_02_15_151927_create_trains_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->tinyInteger('carriages');
             $table->boolean('late');
             $table->boolean('deleted');
+            $table->timestamps();
         });
     }
 
