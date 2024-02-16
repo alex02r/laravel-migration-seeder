f2fecabd734bb70be29c5b501bb458fb9a59bd62
alex02r <132152331+alex02r@users.noreply.github.com>
2024-02-16 10:15:58 +0100

    [FIX] Filtrati i dati con l'order by sull'orario di partenza

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,7 @@ class PageController extends Controller
 {
     public function index(){
         //recuperiamo tutti i movie
-        $trains = Train::all();
+        $trains = Train::orderBy('start_time', 'asc')->get();
 
         return view('welcome', compact('trains'));
     }
